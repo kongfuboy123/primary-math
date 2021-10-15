0cace7f1083fc2fc6432f7415b1cd60f3de6e74c
kongfuboy123 <KONGFUBOYJW2@GMAIL.COM>
2021-10-15 20:08:23 +1100

    增加reset

--- src/pages/math/index.js
+++ src/pages/math/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
 
 class MathTest extends Component {
-  state = {
+  initState = {
     // 初始化习题数组
+    //题库数组
     arr:[],
-    //是否显示答案
-    isAnswer:false,
     //是否显示批改
     isShow : 'hidden',
+    //是否显示答案
+    isAnswer:false,
+   
     
-    
-  }
- 
+  };
+  state = this.initState;
   //生成随机整数(min,max)为生成整数范围
   randomNumber = (min, max) =>{
     const range = max - min + 1;
@@ -20,51 +21,67 @@ class MathTest extends Component {
     return num;
   }
 
-  //加法，n为生成题目数量
-  addition = (n) => {
-    let arr = [];
-    for (let i = 0; i < n; i++){
-      const num1 = this.randomNumber(10,99);
-      const num2 = this.randomNumber(10,99);
-      const sum = num1 + num2
-      arr.push ({
-          index: i,
-          Q: `${num1} + ${num2} =`,
-          A: sum
-      })
-    }
-    return arr
+  getRandomNumber = (n=1) => {
+        return this.randomNumber(10**(n-1),10**n-1);
+  } 
+  
+  
+  //加法
+  addition = (num1, num2) => {
+    
+    const sum = num1 + num2
+      
+    return {
+      Q: `${num1} + ${num2} =`,
+      A: sum}
   }
-  //减法
-  subtraction = (n) => {
+    //减法
+  subtraction = (num1, num2) => {
+    if(num1 >= num2){
+      const diff = num1 - num2
+      return { 
+          Q: `${num1} - ${num2} =`,
+          A: diff
+      }
+    }else{
+      const diff = num2 - num1
+      return{
+          Q: `${num2} - ${num1} =`,
+          A: diff
+      }
+    }
+  }
+  
+  multiplication = (num1, num2) => {
+    
+    const res = num1 * num2
+      
+    return {
+      Q: `${num1} X ${num2} =`,
+      A: res}
+  }
+  /* 题库生成器，n为生成题目数量，func为题目类型,n1,n2 为数字位数，两位数就是2，个位数就1
+  例如两位数加法，func就是addtion n1，n2 都是2 */ 
+  generator = (n,func,n1,n2) =>{
     let arr = [];
     for (let i = 0; i < n; i++){
-      const num1 = this.randomNumber(10,99);
-      const num2 = this.randomNumber(10,99);
-      if(num1 >= num2){
-        const diff = num1 - num2
-        arr.push ({
-            index: i,
-            Q: `${num1} - ${num2} =`,
-            A: diff
-        })
-      }else{
-        const diff = num2 - num1
-        arr.push ({
-            index: i,
-            Q: `${num2} - ${num1} =`,
-            A: diff
-        })
-      }
-     
+      const num1 = this.getRandomNumber(n1);
+      const num2 = this.getRandomNumber(n2);
+      const res = func(num1,num2);
+      arr.push(Object.assign({},{index:i},res))
     }
     return arr
   }
 
+
+
+
+
+
 // 处理input框内填写答案
   handleChange = (e)=>{
     //this.state.arr[e.target.name].A 为题目数组内答案，e.target.value为input内填写的答案
-    console.log(e.target.value)
+   
     if(this.state.arr[e.target.name].A == e.target.value){
       e.target.nextElementSibling.innerText ='✓';
     }else{
@@ -81,17 +98,37 @@ class MathTest extends Component {
       timer = setTimeout(()=>fn.apply(this,args),delay);   
     }
   }
+
+  resetAll = () => { 
+    //reset form
+    document.getElementById("data-box").reset();
+    //reset state
+    this.setState(this.initState);
+  }
   //根据e.target.name，来选择对应的click
   handleClick = (e) => {
-    console.log(e.target.name);
+    
     switch (e.target.name){
-      case 'add':
-        const addArr = this.addition(20);
-        this.setState({arr: addArr});
+      case 'add10':
+        
+        this.resetAll();
+        this.setState({arr: this.generator(10,this.addition,1,1)});
         break;
-      case 'sub':
-        const subArr = this.subtraction(20);
-        this.setState({arr: subArr});
+      case 'add100':
+        this.resetAll();
+        this.setState({arr: this.generator(10,this.addition,2,2)});
+        break;
+      case 'sub10':
+        this.resetAll();
+        this.setState({arr: this.generator(10,this.subtraction,1,1)});
+        break;
+      case 'sub100':
+        this.resetAll();
+        this.setState({arr: this.generator(10,this.subtraction,2,2)});
+        break;
+      case 'multiply ':
+        this.resetAll();
+        this.setState({arr: this.generator(10,this.multiplication,1,1)});
         break;
       case 'answer':
         const flag = !this.state.isAnswer
@@ -102,12 +139,12 @@ class MathTest extends Component {
           this.setState({
             isShow : 'visible'
         })
-        e.target.innerText = '隐藏批改'
+        // e.target.innerText = '隐藏批改'
       }else{
           this.setState({
             isShow : 'hidden'
         })
-        e.target.innerText = '批改作业'
+        // e.target.innerText = '批改作业'
       }
         break;
       default:break;
@@ -116,11 +153,17 @@ class MathTest extends Component {
   }
   render() {
     return (
-      <div>
-        <button name="add" onClick={(e)=>this.handleClick(e)}>加法20道</button>
-        <button name="sub" onClick={(e)=>this.handleClick(e)}>减法20道</button>
-        {/* <div onChange={this.handleChange}> */}
-        <div onChange={this.throttle(this.handleChange,500)}>
+      <div className='container'>
+        <h2 className='title'> 小学数学刷题 </h2>
+        <nav >
+          <button name="add10" onClick={(e)=>this.handleClick(e)}>10以内加法10道</button>
+          <button name="sub10" onClick={(e)=>this.handleClick(e)}>10以内减法10道</button>
+          <button name="add100" onClick={(e)=>this.handleClick(e)}>100以内加法10道</button>
+          <button name="sub100" onClick={(e)=>this.handleClick(e)}>100以内减法10道</button>
+          <button name="multiply " onClick={(e)=>this.handleClick(e)}>乘法10道</button>
+        </nav>
+
+        <form id='data-box' onChange={this.throttle(this.handleChange,500)}>
           Question
           <ul style={{listStyle:'none'}} ref={this.myRef} >
             {this.state.arr.map(data =>
@@ -132,9 +175,9 @@ class MathTest extends Component {
             </li>
             )}
           </ul>
-        </div>
+        </form>
         <div>
-          <button name='check' onClick={this.handleClick}>批改作业</button>
+          <button name='check' onClick={this.handleClick}>{this.state.isShow==='hidden'?'批改作业':'隐藏批改'}</button>
         </div>
         <div style={{visibility:`${this.state.isShow}` }}>
           <button name='answer' onClick={e=>this.handleClick(e)}>Answer</button>
@@ -147,3 +190,5 @@ class MathTest extends Component {
 }
 
 export default MathTest;
+
+
